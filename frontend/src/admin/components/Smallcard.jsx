f6a4ec64f41eeeb646cda1ad2facd97411eb7b6e
Kumar Neupane <3278618@mbmc.edu.np>
2026-01-17 10:22:37 +0545

    working on dashboard main page

---
+++ frontend/src/admin/components/Smallcard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Smallcard = () => {
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="flex flex-col gap-4  ">
+        <h1></h1>
+        <p></p>
+        <p></p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Smallcard;
---
+++ frontend/src/admin/components/StatusBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatusBtn = () => {
+  return (
+    <div>
+      <button type='button' className='rounded-lg bg-gray-200 border-gray-300 p-2'>{}</button>
+    </div>
+  )
+}
+
+export default StatusBtn
